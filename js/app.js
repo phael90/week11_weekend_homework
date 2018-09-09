60dfa772c03e76741db0e18c7185865924c9aaf8
Raphael Ugha <raphaelugha@gmail.com>
2018-09-09 20:51:19 +0100

    Changed the structure in handleDeleteAllData function. MVP competed

--- js/app.js
+++ js/app.js
@@ -8,7 +8,6 @@ document.addEventListener('DOMContentLoaded', () => {
   deleteAll.addEventListener('click', handleDeleteAllData);
 });
 
-
 const handleSubmitForm = ( event ) => {
   event.preventDefault();
 
@@ -34,27 +33,15 @@ const handleSubmitForm = ( event ) => {
   newRow.appendChild(personalNumber);
 
   const personalEmail = document.createElement('td');
-  personalEmail.textContent = `${email}`;
+  personalEmail.textContent = `${emailAddress}`;
   newRow.appendChild(personalEmail);
-
-  console.log(`${firstName}, ${lastName}, ${phoneNumber}, ${emailAddress}`);
   event.target.reset();
-
 }
 
 const handleDeleteAllData = () => {
-  const tablePersonalData1 = document.querySelector('.personal-information');
-  const tablePersonalData2 = document.querySelector('.personal-information');
-  const tablePersonalData3 = document.querySelector('.personal-information');
-  const tablePersonalData4 = document.querySelector('.personal-information');
-  console.log(tablePersonalData1, tablePersonalData2, tablePersonalData3, tablePersonalData4);
+  const tablePersonal = document.querySelector('#personal-information')
 
-  while(tablePersonalData1.firstChild,              tablePersonalData2.firstChild,
-    tablePersonalData3.firstChild,
-    tablePersonalData4.firstChild) {
-      tablePersonalData1.removeChild(tablePersonalData1.firstChild);
-      tablePersonalData2.removeChild(tablePersonalData2.firstChild);
-      tablePersonalData3.removeChild(tablePersonalData3.firstChild);
-      tablePersonalData4.removeChild(tablePersonalData4.firstChild);
+  while(tablePersonal.firstChild) {
+      tablePersonal.removeChild(tablePersonal.firstChild);
     }
   }
