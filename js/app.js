4bee782cc8c7eef55f551c9e0d275248dbc9f630
Raphael Ugha <raphaelugha@gmail.com>
2018-09-09 20:15:03 +0100

    added event listner to form

--- js/app.js
+++ js/app.js
@@ -1,3 +1,6 @@
 document.addEventListener('DOMContentLoaded', () => {
   console.log('js loaded');
+
+  const submitForm = document.querySelector('#form').reset();
+  form.addEventListener('submit', handleSubmitForm);
   });
