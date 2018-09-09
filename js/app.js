3668f95bd110b8fd1c34c8604ec29aae9d19af43
Raphael Ugha <raphaelugha@gmail.com>
2018-09-09 20:19:57 +0100

    populated table

--- js/app.js
+++ js/app.js
@@ -13,4 +13,28 @@ const handleSubmitForm = ( event ) => {
   const lastName = event.target.last.value;
   const phoneNumber = event.target.phone.value;
   const emailAddress = event.target.email.value;
+
+  const newRow = document.createElement('tr');
+  const tablePersonalData = document.querySelector('.personal-information');
+  tablePersonalData.appendChild(newRow);
+
+  const personalFirstName = document.createElement('td');
+  personalFirstName.textContent = `${firstName}`;
+  newRow.appendChild(personalFirstName);
+
+  const personalLastName = document.createElement('td');
+  personalLastName.textContent = `${lastName}`;
+  newRow.appendChild(personalLastName);
+
+  const personalNumber = document.createElement('td');
+  personalNumber.textContent = `${phoneNumber}`;
+  newRow.appendChild(personalNumber);
+
+  const personalEmail = document.createElement('td');
+  personalEmail.textContent = `${email}`;
+  newRow.appendChild(personalEmail);
+
+  console.log(`${firstName}, ${lastName}, ${phoneNumber}, ${emailAddress}`);
+  event.target.reset();
+
 }
