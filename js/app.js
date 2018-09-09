600a4e10978c50672f6320b823517f92f1908ff1
Raphael Ugha <raphaelugha@gmail.com>
2018-09-09 20:16:59 +0100

    added handleSubmitForm function

--- js/app.js
+++ js/app.js
@@ -3,4 +3,14 @@ document.addEventListener('DOMContentLoaded', () => {
 
   const submitForm = document.querySelector('#form').reset();
   form.addEventListener('submit', handleSubmitForm);
-  });
+});
+
+
+const handleSubmitForm = ( event ) => {
+  event.preventDefault();
+
+  const firstName = event.target.first.value;
+  const lastName = event.target.last.value;
+  const phoneNumber = event.target.phone.value;
+  const emailAddress = event.target.email.value;
+}
